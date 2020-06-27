0f9c62ac7e8a921b1d23fe69c03590ead37e8a2b
Laura Ramirez <lau.ramirez.encinas@gmail.com>
2020-06-27 14:42:07 +0200

    first

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
+
       
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
